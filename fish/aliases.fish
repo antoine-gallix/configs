e5a4af0ea03936d1b774e64311a627ec70f8229f
Antoine Gallix <7696611+antoine-gallix@users.noreply.github.com>
2025-05-22 02:21:04 +0300

    reorg aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,10 +1,26 @@
-# short commands
+# ----- navigation -----
+
+# change dir and list
+function c
+    cd $argv
+    ls
+end
 
 # list and variations
 alias l=ls
 alias la="ls -a"
 alias ll="ls -lh"
 alias lla="ls -lha"
+
+# ----- terminal -----
+
+# clear terminal
+alias cl=clear
+
+#kill first job
+alias k='kill %1'
+
+# ----- basic actions -----
 
 # read
 if set -q PAGER
@@ -19,32 +35,21 @@ end
 # open
 alias o=xdg-open
 
-# change dir and list
-function c
-    cd $argv
-    ls
-end
-
-# clear terminal
-alias cl=clear
-
-#kill first job
-alias k='kill %1'
-
-# other aliases
-
-# make file executable by user
-alias makex="chmod u+x"
-
 #clipboard
 if command -q xclip
     alias clip='xclip -selection clip'
 end
 
-# follow a file
-alias follow="tail -f"
-
 # trash files with verbose output
 if command -q trash-put
     alias t='trash-put -v'
 end
+
+# ----- other aliases -----
+
+# follow a file
+alias follow="tail -f"
+
+# make file executable by user
+alias makex="chmod u+x"
+
